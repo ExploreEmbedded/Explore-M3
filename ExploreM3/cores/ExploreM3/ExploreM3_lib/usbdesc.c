ea84e130899382de12a9a8019232ddfe7310228c
SaheblalBagwan <bagwan@exploreembedded.com>
2016-05-16 18:03:58 +0530

    Updated Vid/Pid and DFU bootloader
    Updated Vid/Pid and DFU bootloader

--- ExploreM3/cores/ExploreM3/ExploreM3_lib/usbdesc.c
+++ ExploreM3/cores/ExploreM3/ExploreM3_lib/usbdesc.c
@@ -35,8 +35,8 @@ const uint8_t USB_DeviceDescriptor[] = {
   0x00,                              /* bDeviceSubClass */
   0x00,                              /* bDeviceProtocol */
   USB_MAX_PACKET0,                   /* bMaxPacketSize0 */
-  WBVAL(0x1FC9),                     /* idVendor */
-  WBVAL(0x2002),                     /* idProduct */
+  WBVAL(0x1209),                     /* idVendor */
+  WBVAL(0xEE03),                     /* idProduct */
   WBVAL(0x0100), /* 1.00 */          /* bcdDevice */
   0x01,                              /* iManufacturer */
   0x02,                              /* iProduct */
@@ -141,41 +141,42 @@ const uint8_t USB_StringDescriptor[] = {
   USB_STRING_DESCRIPTOR_TYPE,        /* bDescriptorType */
   WBVAL(0x0409), /* US English */    /* wLANGID */
 /* Index 0x01: Manufacturer */
-  (13*2 + 2),                        /* bLength (13 Char + Type + lenght) */
+  (16*2 + 2),                        /* bLength (13 Char + Type + lenght) */
   USB_STRING_DESCRIPTOR_TYPE,        /* bDescriptorType */
-  'N',0,
+  'E',0,
   'X',0,
   'P',0,
+  'L',0,
+  'O',0,
+  'R',0,
+  'E',0,
   ' ',0,
-  'S',0,
   'E',0,
   'M',0,
-  'I',0,
-  'C',0,
-  'O',0,
-  'N',0,
+  'B',0,
+  'E',0,
   'D',0,
-  ' ',0,
+  'D',0,
+  'E',0,
+  'D',0,
 /* Index 0x02: Product */
-  (17*2 + 2),                        /* bLength ( 17 Char + Type + lenght) */
+  (15*2 + 2),                        /* bLength ( 17 Char + Type + lenght) */
   USB_STRING_DESCRIPTOR_TYPE,        /* bDescriptorType */
-  'N',0,
+  'E',0,
   'X',0,
   'P',0,
-  ' ',0,
   'L',0,
-  'P',0,
-  'C',0,
-  '1',0,
-  '7',0,
-  'x',0,
-  'x',0,
+  'O',0,
+  'R',0,
+  'E',0,
+  ' ',0,
+  'M',0,
+  '3',0,
   ' ',0,
   'V',0,
   'C',0,
   'O',0,
   'M',0,
-  ' ',0,
 /* Index 0x03: Serial Number */
   (12*2 + 2),                        /* bLength (12 Char + Type + lenght) */
   USB_STRING_DESCRIPTOR_TYPE,        /* bDescriptorType */
