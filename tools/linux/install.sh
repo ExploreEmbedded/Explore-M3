2b909fdcb80febe8fb2da5ddd23b73020ad98aaf
Xplorer001 <yes.sandeep@gmail.com>
2016-05-20 17:02:04 +0530

    ExploreM3/system/ExploreM3_lib/lpc17xx.h

---
+++ tools/linux/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if sudo [ -w /etc/udev/rules.d ]; then
+    echo "Copying Explore M3-specific udev rules..."
+    sudo cp -v 90-explore-m3.rules /etc/udev/rules.d/90-explore-m3.rules
+    sudo chown root:root /etc/udev/rules.d/90-explore-m3.rules
+    sudo chmod 644 /etc/udev/rules.d/90-explore-m3.rules
+    echo "Reloading udev rules"
+    sudo udevadm control --reload-rules
+    echo "Adding current user to dialout group"
+    sudo adduser $USER dialout
+else
+    echo "Couldn't copy to /etc/udev/rules.d/; you probably have to run this script as root? Or your distribution of Linux doesn't include udev; try running the IDE itself as root."
+fi
+
