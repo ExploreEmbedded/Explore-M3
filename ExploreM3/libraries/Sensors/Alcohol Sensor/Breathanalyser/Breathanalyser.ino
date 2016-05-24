b4428d2812fdcdd5b6e3d156129dd89e703f34d7
Raghavendra <raghavendra@exploreembedded.com>
2016-05-24 13:51:12 +0530

    Alcohol Sensor code library added

---
+++ ExploreM3/libraries/Sensors/Alcohol Sensor/Breathanalyser/Breathanalyser.ino
@@ -0,0 +1,32 @@
+const int analogPin = A0;    // the pin that the potentiometer is attached to
+const int ledCount = 10;    // the number of LEDs in the bar graph
+
+int ledPins[] = {
+  10,9,8,7,6,5,4,3,2,1 // Here we have the number of LEDs to use in the BarGraph
+  };  
+
+
+void setup() {
+
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT);
+  }}
+
+void loop() {
+  //This is the code to light up LED's
+  int sensorReading = analogRead(analogPin);
+
+  int ledLevel = map(sensorReading, 2000, 4000, 0, ledCount);
+
+
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+
+    if (thisLed < ledLevel) {
+      digitalWrite(ledPins[thisLed], HIGH);
+    }
+
+    else {
+      digitalWrite(ledPins[thisLed], LOW);
+    }  }}
+
+
