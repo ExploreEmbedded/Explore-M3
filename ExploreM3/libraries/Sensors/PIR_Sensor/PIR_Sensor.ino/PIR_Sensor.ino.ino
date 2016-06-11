75934c8b46993e1f462f3cd6c9c0c43ef3221c77
Raghavendra <raghavendra@exploreembedded.com>
2016-06-11 16:18:17 +0530

    8X8 LED matrix code updated

--- ExploreM3/libraries/Sensors/PIR_Sensor/PIR_Sensor.ino/PIR_Sensor.ino.ino
+++ ExploreM3/libraries/Sensors/PIR_Sensor/PIR_Sensor.ino/PIR_Sensor.ino.ino
@@ -1,5 +1,5 @@
-/* 
- * //////////////////////////////////////////////////
+  #include "ST7565.h"
+/* * //////////////////////////////////////////////////
  * //making sense of the Parallax PIR sensor's output
  * //////////////////////////////////////////////////
  *
@@ -26,6 +26,7 @@
  */
 
 /////////////////////////////
+
 //VARS
 //the time we give the sensor to calibrate (10-60 secs according to the datasheet)
 int calibrationTime = 30;        
@@ -40,27 +41,36 @@ long unsigned int pause = 5000;
 boolean lockLow = true;
 boolean takeLowTime;  
 
+
+
 int pirPin = 3;    //the digital pin connected to the PIR sensor's output
 int ledPin = 13;
 
 
+ST7565 glcd(29, 28, 27, 26, 25);
+
 /////////////////////////////
 //SETUP
 void setup(){
-  Serial.begin(9600);
+  
   pinMode(pirPin, INPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(pirPin, LOW);
 
+  glcd.begin(0x18);
+
   //give the sensor some time to calibrate
-  Serial.print("calibrating sensor ");
+  glcd.drawstring(0, 0, " calibrating sensor " ); 
+  glcd.display();     
     for(int i = 0; i < calibrationTime; i++){
-      Serial.print(".");
+      glcd.drawstring(0, 1, ".................." );      
+      glcd.display();     
       delay(1000);
       }
-    Serial.println(" done");
-    Serial.println("SENSOR ACTIVE");
-    delay(50);
+      glcd.clear();
+      glcd.drawstring(20, 0, " SENSOR ACTIVE " );
+      glcd.display();
+   
   }
 
 ////////////////////////////
@@ -71,11 +81,11 @@ void loop(){
        digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
        if(lockLow){  
          //makes sure we wait for a transition to LOW before any further output is made:
-         lockLow = false;            
-         Serial.println("---");
-         Serial.print("motion detected at ");
-         Serial.print(millis()/1000);
-         Serial.println(" sec"); 
+         lockLow = false;
+         glcd.drawstring(0, 2, " motion detected at " );         
+         glcd.drawNumFloat(30,3,millis()/1000,2);         
+         glcd.drawstring(60, 3, " Sec " );
+         glcd.display();        
          delay(50);
          }         
          takeLowTime = true;
@@ -93,10 +103,13 @@ void loop(){
        if(!lockLow && millis() - lowIn > pause){  
            //makes sure this block of code is only executed again after 
            //a new motion sequence has been detected
-           lockLow = true;                        
-           Serial.print("motion ended at ");      //output
-           Serial.print((millis() - pause)/1000);
-           Serial.println(" sec");
+           lockLow = true;            
+           
+           glcd.drawstring(0, 5, " motion ended at " );
+           glcd.drawNumFloat(30,6,(millis() - pause)/1000,2);
+           //Serial.print((millis() - pause)/1000);
+           glcd.drawstring(60, 6, " Sec " );
+           glcd.display();
            delay(50);
            }
        }
